c6af672afd76fada6c6b8aba3ebc41fb15fc622e
Damon Wu <damonwu0605@gmail.com>
2018-09-05 23:44:01 +1000

    ng-study01/feature18: input binding demo

---
+++ src/app/components/input-binding-demo-one/input-binding-demo-one.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-input-binding-demo-one',
+  templateUrl: './input-binding-demo-one.component.html',
+  styleUrls: ['./input-binding-demo-one.component.scss']
+})
+export class InputBindingDemoOneComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
